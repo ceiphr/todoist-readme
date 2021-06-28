cf9c7b095b983854b3a228a995f14e5ed5a2e70a
Ari <ari@ceiphr.com>
2021-06-28 01:35:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -109,8 +109,8 @@ const buildReadme = (prevReadmeContent, newReadmeContent) => {
 
 const commitReadme = async () => {
   // Getting config
-  const committerUsername = "Abhishek Naidu";
-  const committerEmail = "example@gmail.com";
+  const committerUsername = "Ari";
+  const committerEmail = "ari@ceiphr.com";
   const commitMessage = "Todoist updated.";
   // Doing commit and push
   await exec("git", ["config", "--global", "user.email", committerEmail]);
